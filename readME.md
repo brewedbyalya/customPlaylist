5ab98dfe0577865969f3420b3653a326156de877
alya <aliya2002mah@gmail.com>
2025-07-20 11:42:09 +0300

    start

---
+++ readME.md
@@ -0,0 +1,39 @@
+# Custom Playlist Creator
+
+![Site Screenshot](./assets/xxx)
+
+## 🕹️ Description
+A website where the user can create custom playlists.
+
+## 🚀 Getting Started
+
+👉 **Access it** [here!](site)
+
+👉 **Planning Materials:**
+ [User Stories + Wireframe](https://trello.com/b/xmNPFLI4/custom-playlist-creator)
+
+### How to Use
+1. xxx
+2. xxx
+3. xxx
+
+## 💡 Technologies Used
+- HTML
+- CSS
+- Bootstrap
+- JavaScript
+- Node.js
+- Express.js
+- Morgan middleware
+- Mongoose
+- MongoDB
+- EJS
+- Bcrypt
+- Express session
+
+## 📚 Credits / References Used
+- [text](site)
+
+
+## 🚧 Next Steps
+- xxx
---
+++ server.js
@@ -0,0 +1,44 @@
+require('dotenv').config({ quiet: true });
+const express = require('express');
+const app = express();
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const authController = require('./controllers/authController');
+const isSignedIn = require('./middleware/is-signed-in');
+const passUserToView = require('./middleware/pass-user-to-view');
+
+// Database
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+})
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI,
+    })
+}));
+app.use(passUserToView);
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+})
+
+// Routes
+app.use('/auth', authController);
+
+
+const port = process.env.PORT ? process.env.PORT : "3000"
+app.listen(port, () => {
+    console.log(`The express app is ready on port ${port}`);
+});
